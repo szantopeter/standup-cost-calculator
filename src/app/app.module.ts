aeeb6bc9cbea82e31de1973156da97750fe78dbf
Peter Szanto <git@szantocsalad.hu>
2018-10-01 00:15:29 +0200

    Add highchart-angular and basic chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
-
+import { HighchartsChartModule } from "highcharts-angular";
+import { ChartComponent } from "./chart/chart.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ChartComponent],
+  imports: [BrowserModule, HighchartsChartModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/chart/chart.component.ts
@@ -0,0 +1,103 @@
+import { Component, OnInit } from "@angular/core";
+import * as Highcharts from "highcharts";
+
+@Component({
+  selector: "app-chart",
+  templateUrl: "./chart.component.html",
+  styleUrls: ["./chart.component.css"]
+})
+export class ChartComponent implements OnInit {
+  numberOfParticipants = 10;
+  timePerParticipant = 1.5;
+  chart = Highcharts.Chart;
+  Highcharts = Highcharts;
+  chartConstructor = "chart";
+  chartOptions: Highcharts.Options = {
+    title: {
+      text: "Total cost of daily standup"
+    },
+
+    yAxis: {
+      title: {
+        text: "Time (minutes)"
+      },
+      crosshair: true,
+      startOnTick: false,
+      endOnTick: false
+    },
+
+    xAxis: {
+      type: "linear",
+      title: {
+        text: "Number of paricipants"
+      },
+      crosshair: true,
+      tickInterval: 1
+    },
+
+    plotOptions: {
+      area: {
+        fillColor: {
+          linearGradient: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1
+          },
+          stops: [
+            [0, Highcharts.getOptions().colors[3]],
+            [
+              1,
+              Highcharts.Color(Highcharts.getOptions().colors[0])
+                .setOpacity(0)
+                .get("rgba")
+            ]
+          ]
+        },
+        threshold: null
+      }
+    },
+
+    tooltip: {
+      shared: true,
+      headerFormat:
+        '<span style="font-size: 10px">Number of participants : <b>{point.key}</b></span><br/>',
+      valueSuffix: " minutes"
+    },
+    series: []
+  };
+
+  public refreshChart() {
+    let i: number;
+    const actualTime = [];
+    const teamTime = [];
+
+    for (i = 1; i <= this.numberOfParticipants; i++) {
+      actualTime.push({ x: i, y: i * this.timePerParticipant });
+      teamTime.push({ x: i, y: i * i * this.timePerParticipant });
+    }
+
+    const series = [
+      {
+        type: "area",
+        name: "Man hour",
+        data: teamTime
+      },
+      {
+        name: "Actual time",
+        data: actualTime
+      }
+    ];
+
+    this.chartOptions.series = series;
+    this.updateFlag = true;
+  }
+
+  updateFlag = false; // optional boolean
+  oneToOneFlag = true; // optional boolean, defaults to false
+  constructor() {}
+
+  ngOnInit() {
+    this.refreshChart();
+  }
+}
